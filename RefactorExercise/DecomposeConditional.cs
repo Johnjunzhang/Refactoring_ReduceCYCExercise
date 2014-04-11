7e6478a167789fe43cc41abc8c8e4d073942f662
unknown <CYang7@BGC-YANGCHUNXIA.DIR.slb.com>
2014-04-11 10:13:41 +0800

    [CHunXia&LiuYao] init code

---
+++ RefactorExercise/DecomposeConditional.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RefactorExercise
+{
+    [Flags]
+    public enum Weather : short
+    {
+        Null = 0,
+        Sun = 1,
+        Rain = 2,
+        Snow = 4,
+        Wind = 8,
+        Storm = 16
+    }
+
+    public enum Balance
+    {
+        Negative,
+        Zero,
+        Positive
+    }
+
+    public class GeneralView
+    {
+        public DayOfWeek DayofWeek { get; set; }
+        public Weather Weather { get; set; }
+        public Balance Balance { get; set; }
+    }
+
+    public class DecomposeConditional
+    {
+
+        public GeneralView GeneralView { get; set; }
+
+        public bool CanTakeVacation()
+        {
+            return !(GeneralView.Balance == Balance.Negative || GeneralView.Balance == Balance.Zero ||
+                     (GeneralView.Balance == Balance.Positive &&
+                      (GeneralView.Weather == Weather.Rain ||
+                       GeneralView.Weather == (Weather.Rain | Weather.Snow)) &&
+                      GeneralView.DayofWeek == DayOfWeek.Monday) || GeneralView.Weather == Weather.Storm);
+        }
+    }
+}
